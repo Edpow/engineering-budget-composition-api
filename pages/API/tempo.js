2ee1c7b0a131990247d652407f3314075801ecde
Edpow <e_dg_roch@hotmail.com>
2023-07-12 13:03:59 -0400

    testing API routes

--- pages/API/tempo.js
+++ pages/API/tempo.js
@@ -1,9 +1,9 @@
-function handler(req, res) {
+function tempo(req, res) {
     const dynamicDate = new Date();
 
     res.json({
         date: dynamicDate.toISOString()
     })
-}
+};
 
-export default handler;
+export default tempo
