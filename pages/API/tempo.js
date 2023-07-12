0b374fb0ec80c4df63c8e05dd9b6b9957f552ce0
Edpow <e_dg_roch@hotmail.com>
2023-07-12 12:53:34 -0400

    fixing an error in res.status() function

--- pages/API/tempo.js
+++ pages/API/tempo.js
@@ -1,6 +1,6 @@
 function handler(req, res) {
     const dynamicDate = new Date();
-    res.status(200).json({ date: dynamicDate.toLocaleDateString() });
+    res.json({ date: dynamicDate.toLocaleDateString() });
 }
 
 export default handler;
